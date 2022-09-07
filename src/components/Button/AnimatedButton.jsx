ef39e0f575a71f5d4103a2ba3cda0c011f82a99d
Xandowski <xandy-moraes@hotmail.com>
2022-09-07 15:10:00 -0300

    create: Button Component

--- src/components/Button/AnimatedButton.jsx
+++ src/components/Button/AnimatedButton.jsx
@@ -1,13 +1,23 @@
 import styles from './AnimatedButton.module.css'
+import { useState} from 'react'
 
 export function AnimatedButton() {
-  const handleAnimate = (e) => {
-    e.target.classList.toggle('_animate')
+  const [circle, setCircle] = useState(`${styles.circle}`)
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleAnimate = () => {
+    if(isClicked) {
+      setCircle(`${styles.circle} ${styles._circleRight} `)
+      setIsClicked(false)
+    }else {
+      setCircle(`${styles.circle} ${styles._circle}`)
+      setIsClicked(true)
+    }
   }
 
   return (
     <button onClick={handleAnimate} className={styles.animatedBtn} data-js="animate-btn">
-      <div className={styles.circle} data-js="circle"></div>
+      <div className={circle} data-js="circle"></div>
     </button>
   )
 }
--- src/components/Button/ThemeSwitchButton.jsx
+++ src/components/Button/ThemeSwitchButton.jsx
@@ -1,10 +1,14 @@
 import styles from './ThemeSwitchButton.module.css'
+import { useContext, useState } from 'react'
+import { ThemeSwitchContext } from '../../contexts/ThemeSwitchContext'
 
 export function ThemeSwitchButton() {
+  const { light, toggleTheme } = useContext(ThemeSwitchContext)
+  
   return (
-    <button className={styles.themeSwitchBtn}>
-      <img className="sun" src="/src/assets/images/sun.svg" alt="sun" data-js="sun"/>
-      <img className="moon" src="/src/assets/images/moon.svg" alt="moon" data-js="moon"/>
+    <button onClick={() => toggleTheme()} className={styles.themeSwitchBtn}>
+      <img className={`${!light ? styles.sun : styles._sun}`} src="/src/assets/images/sun.svg" alt="sun"/>
+      <img className={`${light ? styles._moon : styles.moon}`} src="/src/assets/images/moon.svg" alt="moon"/>
     </button>
   )
 }
